c1bbab37c8fe82b5c05d3d730248beaa06dd1a2f
arark <knkr5456@gmail.com>
2022-09-23 23:52:22 +0900

    feat(boumn): re-export fp-ts for convinience

---
+++ boumn/src/fp.ts
@@ -0,0 +1,13 @@
+export {
+  array as ARR,
+  either as E,
+  eq as EQ,
+  function as FN,
+  io as IO,
+  option as OP,
+  optionT as OPT,
+  record as REC,
+  string as STR,
+  taskEither as TE,
+  taskOption as TO
+} from "fp-ts";
