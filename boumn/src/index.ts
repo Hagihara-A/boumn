47b1009b8af84e223d8a9d214b5caa2a62cfb7c3
Arark <knkr5456@gmail.com>
2022-09-26 10:12:34 +0900

    chore: WIP

--- boumn/src/index.ts
+++ boumn/src/index.ts
@@ -1,15 +1,19 @@
 import { Errors } from "error";
+import * as pathM from "path";
 
 import { findWsRoot } from "./findWsRoot.js";
-import { E, FN, TE } from "./fp.js";
+import { ARR, E, FN, TE } from "./fp.js";
+import { glob } from "./fs/fs.js";
 import {
+  enumDependentPkgs,
   getAllWsPkgPaths,
   getPackageData,
   getPnpmWsGlob,
   getYarnWsGlob,
+  ordPkg,
   PackageData,
 } from "./getWsInfo.js";
-import { AbsPath, getCwd } from "./path.js";
+import { AbsPath, getCwd, manifestFileName } from "./path.js";
 
 type Config = {
   targetName: string;
@@ -20,15 +24,31 @@ type Config = {
 export type AppEither<T> = E.Either<Errors, T>;
 export type AppTaskEither<T> = TE.TaskEither<Errors, T>;
 
+const config: Config = {
+  destDir: { __tag: "absPath", path: "/home/arark/boumn/bundle" },
+  targetName: "boumn",
+};
+
 export const main = FN.pipe(
   TE.Do,
-  TE.bind("cwd", () => TE.fromIO(getCwd)),
-  TE.bind("wsRoot", ({ cwd }) => findWsRoot(cwd)),
-  TE.bind("wsGlob", ({ wsRoot: { type, path } }) =>
-    type === "pnpm" ? getPnpmWsGlob(path) : getYarnWsGlob(path)
+  TE.bind("cwd", () => (console.log("cwd"), TE.fromIO(getCwd))),
+  TE.bind("wsRoot", ({ cwd }) => (console.log("wsRoot"), findWsRoot(cwd))),
+  TE.bind(
+    "wsGlob",
+    ({ wsRoot: { type, path } }) => (
+      console.log("wsGlob"),
+      type === "pnpm" ? getPnpmWsGlob(path) : getYarnWsGlob(path)
+    )
   ),
-  TE.bind("wsManifestPaths", ({ wsGlob, cwd }) =>
-    getAllWsPkgPaths(cwd, wsGlob)
+  TE.bind(
+    "wsManifestPaths",
+    ({ wsGlob, wsRoot }) => (
+      console.log("wsManifestPaths"),
+      getAllWsPkgPaths(
+        wsRoot.path,
+        ARR.map((pat: string) => pathM.join(pat, manifestFileName))(wsGlob)
+      )
+    )
   ),
 
   TE.bind("packageDataList", ({ wsManifestPaths }) =>
@@ -36,13 +56,33 @@ export const main = FN.pipe(
       wsManifestPaths
     )
   ),
-  TE.chain((a) => {
-    console.log(a.cwd);
-    console.log(a.packageDataList);
-    console.log(a.wsGlob);
-    console.log(a.wsManifestPaths);
-    console.log(a.wsRoot);
+  TE.bind("dependentPackageDataList", () => enumDependentPkgs(ordPkg)()),
+  TE.bind("packageDataWithAllFiles", ({ packageDataList }) =>
+    TE.traverseArray((packageData: PackageData) =>
+      FN.pipe(
+        glob(packageData.path, packageData.files),
+        TE.map((files) => ({ ...packageData, files }))
+      )
+    )(packageDataList)
+  ),
+  (res) => {
+    res()
+      .then((b) => {
+        if (b._tag === "Right") {
+          const a = b.right;
+          const log = (a: unknown, b: unknown) => console.log(a, b, "\n");
+
+          log("cwd", a.cwd);
+          log("wsRoot", a.wsRoot);
+          log("wsGlob", a.wsGlob);
+          log("wsManifestPaths", a.wsManifestPaths);
+          log("packageDataList", a.packageDataList);
+          log("packageDataWithAllFiles", a.packageDataWithAllFiles);
+        }
+        console.log(b);
+      })
+      .catch(console.log);
 
     return TE.right(1);
-  })
+  }
 );
