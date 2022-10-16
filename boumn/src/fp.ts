3fd74e9827837e6b467581255d47e9e60687857e
Arark <knkr5456@gmail.com>
2022-10-16 18:15:58 +0900

    feat: impl `mapRecordWithKey`

--- boumn/src/fp.ts
+++ boumn/src/fp.ts
@@ -14,3 +14,15 @@ export {
   taskEither as TE,
   taskOption as TO,
 } from "fp-ts";
+
+import { array as ARR, function as FN, record as REC } from "fp-ts";
+
+export const mapRecordWithKey =
+  <K1 extends string, V1>(obj: Record<K1, V1>) =>
+  <K2 extends string, V2>(f: (k: K1, v: V1) => [K2, V2]): Record<K2, V2> =>
+    FN.pipe(
+      obj,
+      REC.toEntries,
+      ARR.map(([k, v]) => f(k, v)),
+      REC.fromEntries
+    );
